fee077e265ca0deaee3e39a1185209d79844c220
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-24 10:41:53 +0600

    authentication added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './Pages/Login/Login/Login';
+import Register from './Pages/Login/Register/Register';
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h2>test</h2>
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
     </div>
   );
 }
