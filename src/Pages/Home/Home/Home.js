186bc53ba19a55a1913689f0ac13f11ecc9de1f5
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-24 20:18:49 +0600

    contract and latest news in home page added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Contact from '../Contact/Contact';
 import Footer from '../Footer/Footer';
 import HighlightProducts from '../HighlightProducts/HighlightProducts';
+import LatestNews from '../LatestNews/LatestNews';
 import OurExprience from '../OurExprience/OurExprience';
 import Reviews from '../Reviews/Reviews';
 
@@ -12,6 +14,8 @@ const Home = () => {
             <HighlightProducts />
             <OurExprience />
             <Reviews />
+            <LatestNews />
+            <Contact />
             <Footer />
         </div>
     );
