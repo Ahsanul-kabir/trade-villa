c1a35a3a423bbf30220cb759e169cd13b92ec0c8
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-27 01:06:55 +0600

    product buy added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import BuyNow from './Pages/BuyNow/BuyNow';
 import Dashboard from './Pages/Dashboard/Dashboard/Dashboard';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
 import Register from './Pages/Login/Register/Register';
+import RequireAuth from './Pages/Login/RequireAuth/RequireAuth';
 import Header from './Pages/Shared/Header';
 import NotFound from './Pages/Shared/NotFound';
 
@@ -16,6 +18,11 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/buyNow/:productId" element={
+          <RequireAuth>
+            <BuyNow />
+          </RequireAuth>
+        } />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
