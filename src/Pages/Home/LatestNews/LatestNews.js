6ae8044d297ea45de1e032309b08d03f736e281a
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2022-05-28 23:21:04 +0600

    latest news page updated

--- src/Pages/Home/LatestNews/LatestNews.js
+++ src/Pages/Home/LatestNews/LatestNews.js
@@ -12,33 +12,33 @@ const LatestNews = () => {
                 <Row className="pb-5">
                     <Col xs={12} md={4}>
                         <Card className="mt-4">
-                            <Card.Img style={{ height: '250px' }} variant="top" src="https://i.ibb.co/6wdCY9H/t4.webp" />
+                            <Card.Img style={{ height: '250px' }} variant="top" src="https://i.ibb.co/fvh56yF/1.jpg" />
                             <Card.Body>
-                                <Card.Title>Voltmeter why to use</Card.Title>
+                                <Card.Title>Wire strippers</Card.Title>
                                 <Card.Text>
-                                    A voltmeter is used to read voltage levels and verify that circuits are “live” or off.
+                                    In this category, Ideal Industries' T-Stripper line of wire strippers is a top seller at Graybar.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col xs={12} md={4}>
                         <Card className="mt-4">
-                            <Card.Img style={{ height: '250px' }} variant="top" src="https://i.ibb.co/K71kHDZ/t3.webp" />
+                            <Card.Img style={{ height: '250px' }} variant="top" src="https://i.ibb.co/S6zNwST/2.jpg" />
                             <Card.Body>
-                                <Card.Title>Torpedo Level why to use</Card.Title>
+                                <Card.Title>Fish tapes and poles</Card.Title>
                                 <Card.Text>
-                                    A small level, such as a torpedo level and is used to make sure your work is level.
+                                    Handles on impact-resistant cases are large and comfortable, so grip can be maintained.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
                     <Col xs={12} md={4}>
                         <Card className="mt-4">
-                            <Card.Img style={{ height: '250px' }} variant="top" src="https://i.ibb.co/VmHNSbw/t1.webp" />
+                            <Card.Img style={{ height: '250px' }} variant="top" src="https://i.ibb.co/RBmH4Wd/3.jpg" />
                             <Card.Body>
-                                <Card.Title>Tape Measure why to use</Card.Title>
+                                <Card.Title>Labeling machines</Card.Title>
                                 <Card.Text>
-                                    A standard tape measure is used for all kinds of field measurements.
+                                    Properly labeling work at installation saves time when making moves, adds and changes.
                                 </Card.Text>
                             </Card.Body>
                         </Card>
