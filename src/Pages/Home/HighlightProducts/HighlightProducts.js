48622e05d74155ff230963e7a86d99c9fc1f3c71
Ahsanul-Kabir <ahsanul15-8635@diu.edu.bd>
2023-05-20 13:00:01 +0600

    data loader added

--- src/Pages/Home/HighlightProducts/HighlightProducts.js
+++ src/Pages/Home/HighlightProducts/HighlightProducts.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Product from '../Product/Product';
+import Loading from '../../Shared/Loading';
 
 const HighlightProducts = () => {
     const [products, setProducts] = useState([]);
@@ -12,25 +13,32 @@ const HighlightProducts = () => {
                 setProducts(slice)
             })
     }, [])
-    return (
-        <div className="py-5">
-            <Container>
-                <h1 className="text-center text-uppercase pt-5">TOP TRENDING</h1>
-                <div className="d-flex justify-content-center pb-3">
-                    <hr style={{ width: '70px', height: '5px', marginTop: '0px', background: 'black' }} />
-                </div>
-                <Row>
-                    {
-                        products.map(product => <Product
-                            key={product?._id}
-                            product={product}
-                        ></Product>)
-                    }
-                </Row>
 
-            </Container>
-        </div>
-    );
+    if (products.length === 0) {
+        return (<>
+            <Loading></Loading>
+        </>)
+    } else {
+        return (
+            <div className="py-5">
+                <Container>
+                    <h1 className="text-center text-uppercase pt-5">TOP TRENDING</h1>
+                    <div className="d-flex justify-content-center pb-3">
+                        <hr style={{ width: '70px', height: '5px', marginTop: '0px', background: 'black' }} />
+                    </div>
+                    <Row>
+                        {
+                            products.map(product => <Product
+                                key={product?._id}
+                                product={product}
+                            ></Product>)
+                        }
+                    </Row>
+
+                </Container>
+            </div>
+        );
+    }
 };
 
 export default HighlightProducts;
